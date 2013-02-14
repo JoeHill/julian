ab5300f9728d8fbae862b2fab19252f9c1e87907
Joe Hill <HillJoe1915@gmail.com>
2013-02-14 08:57:39 -0500

    Building parse tree accurately. Beginning to resolve anaphora

--- discourse/services/collins/parser.py
+++ discourse/services/collins/parser.py
@@ -1,19 +1,23 @@
 import os
 import time
 import random
+import hashlib
+import re
 import subprocess
 from subprocess import call
+from nltk.tree import Tree
 import StringIO
 
 from settings import ROOT
 
-ROOT = '/home/joehill/projects/julian/'
+ROOT = '/home/akellehe/projects/julian/'
 
 COLLINS_ROOT = ROOT + 'vendor/COLLINS-PARSER/'
 EVENTS_PATH  = COLLINS_ROOT + 'models/model%s/events.gz'
 PARSER_PATH  = COLLINS_ROOT + 'code/parser'
 GRAMMAR_PATH = COLLINS_ROOT + 'models/model%s/grammar'
 TMP_FILE_DIR = ROOT + 'tmp'
+
 
 def __write_tmp_file(tagged_sents):
     """
@@ -29,9 +33,10 @@ def __write_tmp_file(tagged_sents):
             f.write( __nltk_sent_to_collins_sent(s) + u'\n' )
     return TMP_FILE_DIR + '/' + tmp_file_name
 
+
 def __nltk_sent_to_collins_sent( nltk_sent ):
     """
-    Converts a tagged sentence in NLTK format to a string of the format required by the collins parser.
+    Converts a tagged sentence in NLTK format to a string of the format required by the collins parser. For lookups you can do nltk.help.brown_tagset() or nltk.help.upenn_tagset()
     
     :param list(tuple(str,str)): A pos tagged sentence in NLTK format.
     
@@ -41,10 +46,121 @@ def __nltk_sent_to_collins_sent( nltk_sent ):
     for word, pos in nltk_sent:
         if pos in ( 'AT', 'CS' ):
             pos = 'DT'
-        elif pos is 'CD': # e.g. 20, 100, 10%
+        elif pos == 'CD': # e.g. 20, 100, 10%
             pos = 'JJ'
+        elif pos == 'NR': # Not recognized?
+            pos = 'NN' # Noun. 
+        elif pos == 'PPS': # Pronoun; it
+            pos = 'PRP' # Personal pronoun
+        elif pos == 'HVD': # Have, past tense.
+            pos = 'VBD' # A verb, past tense
+        elif pos == 'PP$': # Determiner, possessive
+            pos = 'PRP$' # pronoun, possessive
+        elif pos == 'OD': # Numeral, ordinal
+            pos = 'JJ' # Adjective
+        else:
+            print "START:" + str( pos ) + ":END"
         simplified += [(word, pos)]
     return unicode( u"%s " % len(simplified) + u" ".join( i for i in [ u"%s %s" % j for j in simplified ]))
+
+
+class CollinsResult:
+    
+    __maximal_depth = 0
+    __tree_refs = {}
+    
+    def __init__(self, s):
+        self.s = s
+        self.length = len(s)
+
+    def parse(self, entry_callback=None, exit_callback=None):
+        nesting = 0
+        token_start = 0
+        token_stop = 0
+        last_entry_result = None
+        last_exit_result = None
+        for i in range(self.length):
+            if self.s[i] == '(':
+                if i+1 < self.length and self.s[i+1] == '/':
+                    continue
+                nesting += 1
+                token_start = i
+                if entry_callback:
+                    last_entry_result = entry_callback((token_start, token_stop), nesting, i)
+            elif self.s[i] == ')':
+                if i+1 < self.length and self.s[i+1] == '/':
+                    continue
+                nesting -= 1
+                token_stop = i+1
+                if exit_callback:
+                    last_exit_result = exit_callback((token_start, token_stop), nesting, i)
+        return ( last_entry_result, last_exit_result )
+        
+
+    def maximal_depth(self):
+        if self.__maximal_depth:
+            return self.__maximal_depth
+
+        nesting = 0
+        get_max = lambda t, n, i: n if n > nesting else nesting
+                
+        self.__maximal_depth, nothing = self.parse( get_max )
+
+        return self.__maximal_depth
+    
+    
+    def get_token_of_length(self, seed, length):
+        hash = hashlib.sha1(seed).hexdigest()
+        if length < 40 and length > 6:
+            return ' >>' + str(hash[0:length-6]) + '<< '
+        elif length > 40:
+            filler = '0' * (length - 6 - 40)
+            return ' >>' + str(hash) + filler + '<< '
+        else:
+            raise Exception( "Bad length for replacement: %s" % length )
+        
+    def node_to_tree(self, node_string ):
+        children = []
+        s = re.split(r'\s+', node_string[1:-1])
+        node_value = s[0].split('~')[0]
+        for tagged_word in s[1:]:
+            if not tagged_word:
+                continue
+            split_word = tagged_word.split('/')
+            if len(split_word) == 1:
+                children.append(self.__tree_refs[tagged_word])
+            else:
+                word, pos = tuple(["".join(split_word[0:-1]), split_word[-1]])
+                children.append( Tree( pos, [word] ) )
+                
+        return Tree( node_value, children )
+        
+        
+    def parse_to_trees(self):
+        maximal_depth = self.maximal_depth()
+        subtree = None
+        for depth in range(maximal_depth,-1,-1):
+            def exit_callback(token, nesting, index):
+                if nesting == depth:
+                    start, stop = token
+                    subtree = self.node_to_tree( self.s[start:stop] )
+                    t = self.get_token_of_length(self.s[start:stop], stop - start )
+                    self.s = self.s[0:start] + t + self.s[stop:]
+                    self.__tree_refs[t.strip()] = subtree
+                    self.__tree = subtree
+            self.parse(exit_callback=exit_callback)
+        
+        print self.__tree
+        return self.__tree
+    
+    
+def collins_to_tree(collins_result_str="()"):
+    """
+        (TOP~confirmed~1~1 (S~confirmed~2~2 (NPB~Korea~2~2 North/NNP Korea/NNP ) (VP~confirmed~4~1 confirmed/VBD (PP~on~2~1 on/IN (NPB~Tuesday~1~1 Tuesday/NNP ) ) (SBAR-A~that~2~1 that/IN (S-A~had~2~2 (NPB~it~1~1 it/PRP ) (VP~had~3~1 had/VBD (VP-A~conducted~2~1 conducted/VBN (NPB~test~5~5 its/PRP$ third/JJ ,/PUNC, long-threatened/NN nuclear/JJ test/NN ,/PUNC, ) ) (PP~according~2~1 according/VBG (PP-A~to~2~1 to/TO (NPB~service~5~5 the/DT official/JJ KCNA/NN news/NN service/NN ,/PUNC, ) ) ) ) ) ) (SG~posing~1~1 (VP~posing~3~1 posing/VBG (NP-A~challenge~2~1 (NPB~challenge~3~3 a/DT new/JJ challenge/NN ) (PP~for~2~1 for/IN (NPB~administration~3~3 the/DT Obama/NN administration/NN ) ) ) (PP~in~2~1 in/IN (NP-A~effort~2~1 (NPB~effort~2~2 its/PRP$ effort/NN ) (SG~to~1~1 (VP~to~2~1 to/TO (VP-A~keep~3~1 keep/VB (NPB~country~2~2 the/DT country/NN ) (PP~from~2~1 from/IN (SG-A~becoming~1~1 (VP~becoming~2~1 becoming/VBG (NPB~power~4~4 a/DT full-fledged/JJ nuclear/JJ power/NN ./PUNC. ) ) ) ) ) ) ) ) ) ) ) ) ) ) 
+    """
+    c = CollinsResult(collins_result_str)
+    return c.parse_to_trees()
+    
 
 def build_trees(tagged_sents, beam_size=1000, punct_constraint=1, 
                 distaflag=1, distvflag=1, npbflag=1, model=2, 
@@ -63,7 +179,6 @@ def build_trees(tagged_sents, beam_size=1000, punct_constraint=1,
     
     :rtype list(Tree): The nltk parse tree for the tagged sentence.
     """
-    print tagged_sents
     tmp_file = __write_tmp_file(tagged_sents)
     
     p1 = subprocess.Popen( [ 'gunzip', '-c', EVENTS_PATH % model ], stdout=subprocess.PIPE )
@@ -81,5 +196,5 @@ def build_trees(tagged_sents, beam_size=1000, punct_constraint=1,
     
     try: os.unlink(tmp_file)
     except: pass
-    print msg
+    print "MESSAGE:", msg, "END"
     return msg
--- fabfile/test.py
+++ fabfile/test.py
@@ -11,60 +11,68 @@ DATABASES_READY = False
 
 @task
 def setup_test_database():
-	global DATABASES_READY
-	if DATABASES_READY:
-		return DATABASES_READY
-	# Set up test settings (database)
-	os.environ['DJANGO_SETTINGS_MODULE'] = 'julian.test_settings'
-	pp = []
-	if 'PYTHONPATH' in os.environ:
-		pp = os.environ['PYTHONPATH'].split( ':' )
-	pp.append( ROOT )
-	pp.append( ROOT.replace( 'julian', '' ) )
-	os.environ['PYTHONPATH'] = ":".join( pp )
-	# Run migrations
-	with lcd(ROOT):
-		# Drop tables
-		call( " ".join( [ 'python', 'manage.py', 'syncdb' ] ), shell=True )
-		call( " ".join( [ 'python', 'manage.py', 'migrate', 'discourse' ] ) , shell=True )
-		DATABASES_READY = True
-	return DATABASES_READY		
+  global DATABASES_READY
+  if DATABASES_READY:
+    return DATABASES_READY
+  # Set up test settings (database)
+  os.environ['DJANGO_SETTINGS_MODULE'] = 'julian.test_settings'
+  pp = []
+  if 'PYTHONPATH' in os.environ:
+    pp = os.environ['PYTHONPATH'].split( ':' )
+  pp.append( ROOT )
+  pp.append( ROOT.replace( 'julian', '' ) )
+  os.environ['PYTHONPATH'] = ":".join( pp )
+  # Run migrations
+  with lcd(ROOT):
+    # Drop tables
+    call( " ".join( [ 'python', 'manage.py', 'syncdb' ] ), shell=True )
+    call( " ".join( [ 'python', 'manage.py', 'migrate', 'discourse' ] ) , shell=True )
+    DATABASES_READY = True
+  return DATABASES_READY    
 
 @task
 def discourse():
-	"""Run discourse tests."""
-	if setup_test_database():
-		test_path = ROOT + '/discourse/tests/'
-		test_filenames = os.listdir(test_path)
-		paths = [ test_path + f for f in test_filenames ]
-		with lcd(ROOT):
-			for p in paths:
-				call( " ".join( [ 'python', p ] ) )
-				
+  """Run discourse tests."""
+  if setup_test_database():
+    test_path = ROOT + '/discourse/tests/'
+    test_filenames = os.listdir(test_path)
+    paths = [ test_path + f for f in test_filenames ]
+    with lcd(ROOT):
+      for p in paths:
+        call( " ".join( [ 'python', p ] ) )
+        
 @task
 def node():
-	"""Run node tests"""
-	if setup_test_database():
-		with lcd(ROOT):
-			call( " ".join( ['python', 'discourse/tests/node_test.py'] ), shell=True )
+  """Run node tests"""
+  if setup_test_database():
+    with lcd(ROOT):
+      call( " ".join( ['python', 'discourse/tests/node_test.py'] ), shell=True )
 
 @task
 def edge():
-	"""Run edge tests"""
-	if setup_test_database():
-		with lcd(ROOT):
-			call( " ".join( ['python', 'discourse/tests/edge_test.py'] ), shell=True )		
-			
+  """Run edge tests"""
+  if setup_test_database():
+    with lcd(ROOT):
+      call( " ".join( ['python', 'discourse/tests/edge_test.py'] ), shell=True )    
+      
 @task
 def note():
-	"""Run note tests"""
-	if setup_test_database():
-		with lcd(ROOT):
-			call( " ".join( ['python', 'discourse/tests/note_test.py'] ), shell=True )
+  """Run note tests"""
+  if setup_test_database():
+    with lcd(ROOT):
+      call( " ".join( ['python', 'discourse/tests/note_test.py'] ), shell=True )
 
 @task
 def inference():
-	"""Run interence tests"""
-	if setup_test_database():
-		with lcd(ROOT):
-			call( " ".join( ['python', 'discourse/tests/inference_test.py'] ), shell=True )
+  """Run interence tests"""
+  if setup_test_database():
+    with lcd(ROOT):
+      call( " ".join( ['python', 'discourse/tests/inference_test.py'] ), shell=True )
+
+@task
+def collins_parser():
+  """Run collins parser test"""
+  if setup_test_database():
+    with lcd(ROOT):
+      call( " ".join( ['python', 'discourse/tests/collins_parser_test.py'] ), shell=True )
+
